16cf98b04e04f542f360f7661d67e64b575cddf1
bsubbaraman <bms12014@mymail.pomona.edu>
2026-06-16 12:55:17 -0700

    add message about webserial support on browser

--- src/lib/events/connectToMachine.js
+++ src/lib/events/connectToMachine.js
@@ -1,5 +1,11 @@
 import { evalCode } from "$lib/repl";
+import { editorState } from "../../store/state.svelte.js";
 
 export function connectToMachine() {
+    if (!('serial' in navigator)) {
+        editorState.printAlert =
+            'Connecting to a printer needs the Web Serial API, which works in Chrome, Edge, or another Chromium browser. You can still edit code and save g-code here.';
+        return;
+    }
     evalCode(`fab.serial.requestPort()`);
 }
